752270dbf15e7391d0fb27c1c75308c4028527f4
Nugroho Arief Widodo <nawidodo@gmail.com>
2019-03-27 01:48:30 +0700

    Initial Commit

---
+++ Sempoaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sempoaa
+//
+//  Created by Nugroho Arief Widodo on 27/03/19.
+//  Copyright © 2019 KitaBisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
